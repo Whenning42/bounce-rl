2670c59117f31d16344059288c76dfccc11d6dd4
William Henning <wchenningwchenning@gmail.com>
2021-02-11 17:33:00 -0500

    Switched to integer observation names

--- model.py
+++ model.py
@@ -16,30 +16,36 @@ class Model(object):
         self.name = datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S:%f')
         os.mkdir("memories/" + self.name)
         self.last_action = np.zeros(84)
-        self.startup = 0
+        self.frame = 0
 
     def save_state(self, bitmap, keymap):
-        timestamp = datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S:%f')
+        filename = str(self.frame)
+        save_dir = "memories/" + self.name + "/"
 
         im = Image.fromarray(bitmap)
-        im.save("memories/" + self.name + "/" + timestamp + ".png")
+        im.save(save_dir + filename + ".png")
 
-        f = open("memories/" + self.name + "/" + timestamp + ".keymap", "w")
-        keymap.astype('uint8').tofile(f)
+        keymap_file = open(save_dir + filename + ".keymap", "w")
+        keymap.astype('uint8').tofile(keymap_file)
+
+        timestamp = datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S:%f')
+        timestamp_file = open(save_dir + filename + ".timestamp", "w")
+        timestamp_file.write(timestamp)
 
     def update(self, state):
         action_keymap = np.zeros(84)
+        self.frame += 1
 
-        # We use a no-op model here to get a user trial
+        # We use a no-op model here to get a user image data.
         self.save_state(state, action_keymap)
         return action_keymap
 
-        if self.startup < 20 * 15:
+        # Hardcode pressing enter? to get through menu screen.
+        if self.frame < 20 * 15:
             if int(time.time() * 10) % 2 == 0:
                 action_keymap[57] = 1
             else:
                 action_keymap[57] = 0
-            self.startup += 1
 
         elif 0 == 0:
             action_keymap[random.randint(0, 83)] = 1
