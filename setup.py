5e8e076ab6ff8fd8b55145dcc8c54674395ffeac
William Henning <wchenningwchenning@gmail.com>
2022-01-01 12:37:34 -0500

    Deleted unused ROS integration

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ import numpy
 setup(
     ext_modules = cythonize(
         [Extension("image_capture", ["image_capture.pyx"], libraries=["image_capture"], include_dirs=[numpy.get_include()]),
-         Extension("camera_node", ["camera_node.pyx"], libraries=["camera_node"], include_dirs=[numpy.get_include()])]
         )
 )
