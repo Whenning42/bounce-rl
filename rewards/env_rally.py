56b35bf56d54dea98c7e674a5b2db8f058816d0f
William Henning <wchenningwchenning@gmail.com>
2022-04-15 17:57:57 -0400

    Make art of rally env support multi-instance running

--- rewards/env_rally.py
+++ rewards/env_rally.py
@@ -1,5 +1,5 @@
 import rewards
-import rewards.art_of_rally
+# import rewards.art_of_rally
 import src.time_writer
 import time
 import app_configs
@@ -7,22 +7,24 @@ from harness import Harness
 import gym
 import numpy as np
 import callbacks.callbacks as callbacks
+import threading
 
 DOWNSAMPLE = 2
 
 class ArtOfRallyEnv(gym.core.Env):
-    def __init__(self, out_dir = None):
+    def __init__(self, out_dir = None, channel = 0):
         X_RES = 960
         Y_RES = 540
-        art_of_rally_reward_callback = rewards.art_of_rally.ArtOfRallyReward(plot_output = False)
-        screenshot_callback = callbacks.ScreenshotCallback(out_dir = out_dir)
+        # art_of_rally_reward_callback = rewards.art_of_rally.ArtOfRallyReward(plot_output = False)
+        # screenshot_callback = callbacks.ScreenshotCallback(out_dir = out_dir)
         run_config = {
             "title": "Art of Rally reward eval",
-            "app": "Art of Rally",
+            "app": "Art of Rally (Multi)",
             "max_tick_rate": None,
             "x_res": 1920,
             "y_res": 1080,
             "scale": .5,
+            "row_size": 2,
             "run_rate": 8,
             "pause_rate": .25,
             "step_duration": .250,
@@ -30,14 +32,16 @@ class ArtOfRallyEnv(gym.core.Env):
         self.run_config = run_config
         app_config = app_configs.LoadAppConfig(run_config["app"])
 
-        harness = Harness(app_config, run_config)
-        art_of_rally_reward_callback.attach_to_harness(harness)
-        screenshot_callback.attach_to_harness(harness)
+        harness = Harness(app_config, run_config, instance = channel)
+        # art_of_rally_reward_callback.attach_to_harness(harness)
+        # screenshot_callback.attach_to_harness(harness)
 
         self.harness = harness
         # Reward callback is called by env.
-        self.reward_callback = art_of_rally_reward_callback
-        self.screenshot_callback = screenshot_callback
+        # self.reward_callback = art_of_rally_reward_callback
+        self.reward_callback = lambda: None
+        self.reward_callback.on_tick = lambda: (0, 0, 0)
+        # self.screenshot_callback = screenshot_callback
 
         self.action_space = gym.spaces.MultiDiscrete([3, 3])
         # Input space is in xlib XK key strings with XK_ left off.
@@ -53,12 +57,41 @@ class ArtOfRallyEnv(gym.core.Env):
         self.episode_steps = 0
         self.total_steps = 0
 
-        self.wait_for_harness_init()
+        self.env_init = False
+        # The setup thread sets self.env_init to True once the setup is finished.
+        setup_thread = threading.Thread(target = self._setup_env_async, args = (), kwargs = {})
+        setup_thread.start()
 
-    def wait_for_harness_init(self):
+    def _wait_for_env_init(self):
+        while self.env_init == False:
+            time.sleep(.5)
+            print("Waiting for env setup")
+
+    def _setup_env_async(self):
+        # Wait for the harness to be initialized
         while self.harness.ready == False:
             self.harness.tick()
             time.sleep(.5)
+            print("Waiting for harness")
+
+        # Run the keypresses necessary to get past the menu
+        sequence = ((10, "Return"),
+                    (.4, "Down"),
+                    (.4, "Return"),
+                    (.4, "Down"),
+                    (.4, "Right"),
+                    (.4, "Return"),
+                    (.4, "Return"),
+                    (15, "Return"),
+                    (.4, "Return"),
+                    (3, "Return"))
+        for t, key in sequence:
+            time.sleep(t)
+            self.harness.keyboards[0].key_sequence((key,))
+
+        print("Finished launching an episode")
+        self.env_init = True
+
 
     def render(self):
         print("ArtOfRallyEnv.render is unimplemented.")
@@ -66,7 +99,7 @@ class ArtOfRallyEnv(gym.core.Env):
         pass
 
     def reset(self):
-        self.wait_for_harness_init()
+        self._wait_for_env_init()
         self.episode_steps = 0
 
         self.harness.keyboards[0].set_held_keys(set())
@@ -84,7 +117,7 @@ class ArtOfRallyEnv(gym.core.Env):
         pass
 
     def step(self, action):
-        self.wait_for_harness_init()
+        self._wait_for_env_init()
 
         # Run keyboard presses for the given the gym action.
         key_set = set()
@@ -93,8 +126,8 @@ class ArtOfRallyEnv(gym.core.Env):
                 continue
             key_set.add(self.input_space[i][v - 1])
         self.harness.keyboards[0].set_held_keys(key_set)
-        if self.total_steps % 100 == 0:
-            self.screenshot_callback.on_tick()
+        # if self.total_steps % 100 == 0:
+        #     self.screenshot_callback.on_tick()
 
         src.time_writer.SetSpeedup(self.run_config["run_rate"])
         time.sleep(self.run_config["step_duration"] / self.run_config["run_rate"])
