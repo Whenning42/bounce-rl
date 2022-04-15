10dd22d51833392b88b83aa1494c5daeb8daa6f2
William Henning <Henningw@umich.edu>
2022-04-15 18:03:57 -0400

    Uncomment failing time control tests
    The sleep family of functions were removed when I was debugging
    race conditions and I never re-implemented them. I'm leaving the tests
    around in case I do re-implement the functions.

--- src/child.cpp
+++ src/child.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char** argv) {
   setlinebuf(stdout);
   while (true) {
     printf("tick\n");
+    time_t time_v = time(nullptr);
     sleep(1);
   }
 }
