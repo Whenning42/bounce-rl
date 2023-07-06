b3f2036380e3783cfad0044d7bdd2ef84665bc0b
William Henning <wchenningwchenning@gmail.com>
2023-07-05 22:29:05 -0400

    Move scripts into a scripts folder

--- scripts/ld_run.sh
+++ scripts/ld_run.sh
@@ -1,3 +1,2 @@
-# Runs the given command with the correct ld
-# library path configured
+# Runs the given command with the correct ld library path configured
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./ PYTHONPATH=../stable-baselines3:$PYTHONPATH "$@"
