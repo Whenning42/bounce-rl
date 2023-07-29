24f75eabc033c3837b90341c35fad35bcb27b51d
William Henning <Henningw@umich.edu>
2023-07-29 07:40:49 -0400

    Add AoR profiling and update AoR to put gas and brake on single axis

--- scripts/sb3_aor.py
+++ scripts/sb3_aor.py
@@ -19,9 +19,8 @@ def PPO(out_dir, env, seed = 0, n_steps = 2048, ent_coef = .01):
     return stable_baselines3.PPO('CnnPolicy', env, verbose = 1, device = "cuda:0", tensorboard_log = os.path.join(out_dir, "tensorboard_out"), seed = seed, n_steps = n_steps, ent_coef = ent_coef)
 
 @gin.configurable
-def SAC(out_dir, env, seed = 0, ent_coef = .1, profiler = None):
-    return stable_baselines3.SAC('CnnPolicy', env, verbose = 1, device = "cuda:0", tensorboard_log = os.path.join(out_dir, "tensorboard_out"), seed = seed, optimize_memory_usage = True, buffer_size = 400000, learning_starts=10000, ent_coef = ent_coef, train_freq=50, gradient_steps=50, profiler=profiler)
-    # return stable_baselines3.SAC('CnnPolicy', env, verbose = 1, device = "cuda:0", tensorboard_log = os.path.join(out_dir, "tensorboard_out"), seed = seed, optimize_memory_usage = True, buffer_size = 50000, optimizer_class = torch.optim.SGD, optimizer_kwargs={"lr": 1e-4, "momentum": .9})
+def SAC(out_dir, env, seed = 0, ent_coef = .1, profiler = None, train_start = None, train_end = None):
+    return stable_baselines3.SAC('CnnPolicy', env, verbose = 1, device = "cuda:0", tensorboard_log = os.path.join(out_dir, "tensorboard_out"), seed = seed, optimize_memory_usage = True, buffer_size = 400000, learning_starts=5000, ent_coef = ent_coef, train_freq=50, gradient_steps=50, profiler=profiler, profiler_cls=Profiler, on_training_start=train_start, on_training_end=train_end)
 
 @gin.configurable
 def run(out_dir = "out/run/", seed = 0, timesteps = 2e6, n_stack = 4):
@@ -33,10 +32,13 @@ def run(out_dir = "out/run/", seed = 0, timesteps = 2e6, n_stack = 4):
         print(gin.config_str())
         f.write(gin.config_str())
 
-    p = Profiler("./run_profile2.csv")
+    p = Profiler("./run_profile_16.csv")
+    p.begin("Env Step")
+    p.begin("Framework", end="Env Step")
     # Create environment
     gamma = .985
-    orig_env = rewards.env_rally.ArtOfRallyEnv(out_dir = out_dir, gamma=gamma, run_rate=12, profiler=p)
+    orig_env = rewards.env_rally.ArtOfRallyEnv(out_dir = out_dir, gamma=gamma, run_rate=4, pause_rate=1, profiler=p)
+    train_start, train_end = orig_env.get_train_start(), orig_env.get_train_end()
     env = VecFrameStack(DummyVecEnv([lambda: orig_env]), n_stack = n_stack)
 
     eval_env = env
@@ -44,14 +46,14 @@ def run(out_dir = "out/run/", seed = 0, timesteps = 2e6, n_stack = 4):
     env = stable_baselines3.common.vec_env.VecMonitor(env)
 
     callback_nsteps = 30000
-    eval_callback = stable_baselines3.common.callbacks.EvalCallback(eval_env, eval_freq = callback_nsteps, log_path = out_dir, n_eval_episodes = 3)
+    eval_callback = stable_baselines3.common.callbacks.EvalCallback(eval_env, eval_freq = callback_nsteps, log_path = out_dir, n_eval_episodes = 3, deterministic=False)
     checkpoint_callback = stable_baselines3.common.callbacks.CheckpointCallback(save_freq = callback_nsteps, save_path = out_dir)
 
     # Instantiate the agent.
     if discrete:
         model = PPO(out_dir, env)
     else:
-        model = SAC(out_dir, env, ent_coef = .1 * (1-gamma), profiler=p)
+        model = SAC(out_dir, env, ent_coef = .1 * (1-gamma), profiler=p, train_start=train_start, train_end=train_end)
 
     print("Ready to train with operative config: ", gin.operative_config_str())
     # Frame stack breaks check_env?
@@ -67,7 +69,7 @@ gin.parse_config_file('config.gin')
 
 if __name__ == "__main__":
     for seed, n_steps in itertools.product((0,), (4800,)):
-        gin.bind_parameter("run.out_dir", f"out/take_three/n_steps_{n_steps}_seed_{seed}")
+        gin.bind_parameter("run.out_dir", f"out/take_16/n_steps_{n_steps}_seed_{seed}")
         gin.bind_parameter("run.seed", seed)
         gin.bind_parameter("PPO.seed", seed)
         gin.bind_parameter("SAC.seed", seed)
