9b2796d067d9f2c69d2876001c26e6c69de64301
William Henning <wchenningwchenning@gmail.com>
2023-08-18 23:05:54 -0400

    Rename lua output files from csv to tsv to reflect their content.

--- rewards/noita_info.py
+++ rewards/noita_info.py
@@ -1,6 +1,5 @@
 import os
 
-import csv
 from typing import Optional
 from pathlib import Path
 
@@ -41,7 +40,7 @@ class NoitaInfo:
 
         # Keep in sync with Noita mod.
         self.info = {"biome": "", "hp": 100, "max_hp": 100, "gold": 0, "x": 0, "y": 0}
-        self.info_file = os.path.join(pipe_dir, "noita_stats.csv")
+        self.info_file = os.path.join(pipe_dir, "noita_stats.tsv")
         self.info_tail = FileTail(
             self.info_file, "a+", ", ".join([str(v) for v in self.info.values()])
         )
@@ -49,11 +48,15 @@ class NoitaInfo:
         self.notification_file = os.path.join(pipe_dir, "noita_notifications.txt")
         self.notification_tail = FileTail(self.notification_file, "a+")
 
+        # Clear any existing notifications.
+        self.on_tick()
+        self.is_alive = True
+
     def on_tick(self) -> dict:
         # Update info
         new_vals_line, new_data = self.info_tail.get()
         self.is_alive = new_data or self.is_alive
-        new_vals = next(csv.reader([new_vals_line]))
+        new_vals = new_vals_line.split('\t')
         for k, v in zip(self.info.keys(), new_vals):
             self.info[k] = v
 
