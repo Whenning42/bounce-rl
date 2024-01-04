8e0228401d350f87dc4461b661e8cac6d3090d86
William Henning <Henningw@umich.edu>
2024-01-03 22:08:45 -0500

    Formatting fixes for noita_sb3.py

--- src/scripts/noita_sb3.py
+++ src/scripts/noita_sb3.py
@@ -31,9 +31,9 @@ def PPO(out_dir, env, seed=0, n_steps=2048, ent_coef=0.01):
         n_steps=n_steps,
         ent_coef=ent_coef,
         n_epochs=4,
-        target_kl=.03,
+        target_kl=0.03,
         learning_rate=4e-5,
-        batch_size=128
+        batch_size=128,
     )
 
 
