3c0fb61570853f8668db990091ae08ab58e12d05
William Henning <Henningw@umich.edu>
2024-03-14 22:02:59 -0400

    Add mock_env and mock_env sb3 integration test

---
+++ rewards/mock_env.py
@@ -0,0 +1,100 @@
+# A mock gym environment for integration testing.
+
+import os
+import time
+from enum import Enum
+from typing import Any, Iterable, Optional
+
+import gym
+import numpy as np
+
+import configs.app_configs as app_configs
+import keyboard
+import rewards.noita_info
+import rewards.noita_reward
+import src.time_writer
+import torch
+from harness import Harness
+
+
+class MockEnv(gym.core.Env):
+    def __init__(
+        self,
+        out_dir: Optional[str] = None,
+        # We're not yet launching noita with time control LD_PRELOAD. Once we do,
+        # we can set run_rate and pause_rate to 4 and 0.25 respectively.
+        run_rate: float = 1,
+        pause_rate: float = 1,
+        env_conf: Optional[dict] = None
+    ):
+        self.out_dir = out_dir
+        self.run_rate = run_rate
+        self.pause_rate = pause_rate
+        if env_conf is None:
+            env_conf = {}
+        self.env_conf = env_conf
+        run_config = {
+            "app": "Noita",
+            "x_res": 640,
+            "y_res": 360,
+            "scale": 1,
+            "run_rate": run_rate,
+            "pause_rate": pause_rate,
+            "step_duration": 0.25,
+            "pixels_every_n_episodes": 1,
+        }
+        self.run_config: dict[str, Any] = run_config
+        self.app_config = app_configs.LoadAppConfig(run_config["app"])
+
+        self.ep_step = 0
+        self.env_step = 0
+
+        # Action and observation space copied from Noita env.
+        self.input_space = [
+            ("W", "S"),
+            ("A", "D"),
+            ("F",),
+            ("E",),
+            ("1",),
+            ("5",),
+            (keyboard.MouseButton.LEFT, keyboard.MouseButton.RIGHT),
+        ]
+        self.input_space = [x + (None,) for x in self.input_space]
+        discrete_lens = [len(x) for x in self.input_space]
+        self.action_space = gym.spaces.Tuple(
+            (
+                gym.spaces.MultiDiscrete(discrete_lens),
+                gym.spaces.Box(low=-1, high=1, shape=(2,)),
+            )
+        )
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(self.run_config["y_res"], self.run_config["x_res"], 3), dtype=np.uint8
+        )
+
+    # Stable baselines3 requires a seed method.
+    def seed(self, seed):
+        pass
+
+    # SB3 expects `done` instead of `terminated` and `truncated`.
+    # def step(self, action: tuple[Iterable, Iterable]) -> tuple[np.ndarray, float, bool, bool, dict]:
+    def step(self, action: tuple[Iterable, Iterable]) -> tuple[np.ndarray, float, bool, dict]:
+        self.ep_step += 1
+        self.env_step += 1
+
+        pixels = torch.zeros((self.run_config["y_res"], self.run_config["x_res"], 3), dtype=torch.uint8)
+        info = {}
+        reward = 0
+        terminated = False
+        truncated = False
+        # return pixels, reward, terminated, truncated, info
+        done = terminated
+        return pixels, reward, done, info 
+
+    # SB3 doesn't handle info returned in reset method.
+    # def reset(self, *, seed: Any = None, options: Any = None) -> tuple[gym.core.ObsType, dict]:
+    def reset(self, *, seed: Any = None, options: Any = None) -> gym.core.ObsType:
+        """Seed isn't yet implemented. Options are ignored."""
+        pixels = torch.zeros((self.run_config["y_res"], self.run_config["x_res"], 3), dtype=torch.uint8)
+        info = {}
+        # return pixels, info
+        return pixels
---
+++ src/scripts/mock_sb3.py
@@ -0,0 +1,58 @@
+# Integration test between mock env and SB3.
+
+import gym
+import stable_baselines3.common.env_checker
+import stable_baselines3
+from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage, VecFrameStack, VecMonitor
+import rewards.mock_env
+import os
+import gin
+import pathlib
+import itertools
+import time
+import torch
+
+discrete = False
+
+@gin.configurable
+def PPO(out_dir, env, seed = 0, n_steps = 2048, ent_coef = .01):
+    return stable_baselines3.PPO('CnnPolicy', env, verbose = 1, device = "cuda:0", tensorboard_log = os.path.join(out_dir, "tensorboard_out"), seed = seed, n_steps = n_steps, ent_coef = ent_coef)
+
+@gin.configurable
+def run(out_dir = "out/run/", seed = 0, timesteps = 1e6, n_stack = 3):
+    out_dir = os.path.join(out_dir, str(seed))
+    pathlib.Path(out_dir).mkdir(parents= True, exist_ok = True)
+
+    orig_env = rewards.mock_env.MockEnv(out_dir = out_dir)
+    env = VecFrameStack(DummyVecEnv([lambda: orig_env]), n_stack = n_stack)
+
+    eval_env = env
+    # Makes episode length and mean reward visible in tensorboard logging.
+    env = stable_baselines3.common.vec_env.VecMonitor(env)
+
+    callback_nsteps = 30000
+    eval_callback = stable_baselines3.common.callbacks.EvalCallback(eval_env, eval_freq = callback_nsteps, log_path = out_dir, n_eval_episodes = 3, deterministic=False)
+    checkpoint_callback = stable_baselines3.common.callbacks.CheckpointCallback(save_freq = callback_nsteps, save_path = out_dir)
+
+    # Instantiate the agent.
+    model = PPO(out_dir, env)
+
+    print("Ready to train with operative config: ", gin.operative_config_str())
+    # Frame stack breaks check_env?
+    # stable_baselines3.common.env_checker.check_env(env)
+    model.learn(total_timesteps = timesteps, callback = [eval_callback, checkpoint_callback])
+    exit()
+
+    # SB3 doesn't close the env after learn call?
+    orig_env.close()
+    time.sleep(1)
+
+# Move config into a gin file if it grows too big. See sb3_aor.py for an example.
+if __name__ == "__main__":
+    for seed, n_steps in itertools.product((0,), (4000,)):
+        gin.bind_parameter("run.out_dir", f"out/noita_sb3_ppo_no_reg/n_steps_{n_steps}_seed_{seed}")
+        gin.bind_parameter("run.seed", seed)
+        gin.bind_parameter("PPO.seed", seed)
+        gin.bind_parameter("PPO.n_steps", n_steps)
+
+        run(n_stack = 4)
