719207ba44585e2c254e755140bd5d00c83dea1f
William Henning <Henningw@umich.edu>
2024-04-02 16:38:44 -0400

    Add image capture

---
+++ bounce_rl/core/image_capture/image_capture.c
@@ -0,0 +1,87 @@
+#include "image_capture.h"
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XShm.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+#include <stdio.h>
+
+struct ImageCapture {
+  Display *display;
+  int screen;
+  XImage *image;
+  XShmSegmentInfo shminfo;
+  void* handler;
+  int x;
+  int y;
+};
+
+capture_t SetupImageCapture(int x, int y, int width, int height) {
+  struct ImageCapture* capture = malloc(sizeof(struct ImageCapture));
+  capture->x = x;
+  capture->y = y;
+
+  Display *display = XOpenDisplay(NULL);
+  int screen = XDefaultScreen(display);
+
+  XImage *image =
+      XShmCreateImage(display, DefaultVisual(display, screen), 24, ZPixmap,
+                      NULL, &capture->shminfo, width, height);
+
+  // Creates a new shared memory segment large enough for the image with read
+  // write permissions
+  capture->shminfo.shmid = shmget(IPC_PRIVATE, image->bytes_per_line * image->height,
+                         IPC_CREAT | S_IRWXU);
+  capture->shminfo.readOnly = False;
+
+  assert(capture->shminfo.shmid != -1);
+
+  image->data = (char *)shmat(capture->shminfo.shmid, NULL, 0);
+  capture->shminfo.shmaddr = image->data;
+
+  XShmAttach(display, &capture->shminfo);
+  capture->display = display;
+  capture->screen = screen;
+  capture->image = image;
+
+  return capture;
+}
+
+#include <time.h>
+char *CaptureImage(const capture_t capture_h, Window window) {
+  const struct ImageCapture* capture = capture_h;
+
+  clock_t start = clock();
+  XShmGetImage(capture->display, window, capture->image, capture->x, capture->y, AllPlanes);
+  double duration = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+
+  return capture->image->data;
+}
+
+void CleanupImageCapture(capture_t capture_h) {
+    struct ImageCapture* capture = capture_h;
+    assert(XShmDetach(capture->display, &capture->shminfo));
+    XDestroyImage(capture->image);
+    shmdt(capture->shminfo.shmaddr);
+    shmctl(capture->shminfo.shmid, IPC_RMID, 0);
+    XCloseDisplay(capture->display);
+    free(capture);
+}
+
+int (*global_mim)(Display*, XErrorEvent*, void*) = NULL;
+void* global_py_handler = NULL;
+
+#include <stdio.h>
+int _call_global_handler(Display* display, XErrorEvent* error) {
+    return global_mim(display, error, global_py_handler);
+}
+
+void SetErrorHandler(OnErrorMIM mim, void* on_error_py) {
+  global_mim = mim;
+  global_py_handler = on_error_py;
+  XSetErrorHandler(_call_global_handler);
+}
