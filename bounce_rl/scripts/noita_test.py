aa00e9657c4acbc9b9d441844cd1717f5a01f240
William Henning <wchenningwchenning@gmail.com>
2024-04-28 19:16:59 -0400

    Fix issues in noita_test.py
    The file had an incorrect import path and was using the 5-argument
    step() signature instead of the 4-argument one that Bounce is still
    on (return `terminated` and `truncated` or just `done` respectively).

--- bounce_rl/scripts/noita_test.py
+++ bounce_rl/scripts/noita_test.py
@@ -1,11 +1,20 @@
-import rewards.noita_env as noita_env
-
 import numpy as np
 
+from bounce_rl.environments.noita import noita_env
+
 if __name__ == "__main__":
+    noita_env.NoitaEnv.pre_init()
     env = noita_env.NoitaEnv()
+
+    terminated = False
     while True:
-        pixels, reward, terminated, truncated, info = env.step(env.action_space.sample())
-        print(f"mean_pixels: {np.mean(pixels)}, r: {reward}, done: {terminated}, info {info}")
+        ret = env.step(
+            env.action_space.sample()
+        )
+        if ret is not None:
+            pixels, reward, done, info = ret
+            print(
+                f"mean_pixels: {np.mean(pixels)}, r: {reward}, done: {done}, info {info}"
+            )
         if terminated:
             env.reset()
