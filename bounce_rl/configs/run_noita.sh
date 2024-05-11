c2e92ce696d3e7673c95b0ee48a58206dbfe1985
William Henning <wchenningwchenning@gmail.com>
2024-05-11 17:44:20 -0400

    Make run_noita.sh path names more clearly correspond to proton directories

--- bounce_rl/configs/run_noita.sh
+++ bounce_rl/configs/run_noita.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
-# Generated with `lutris -b run_noita.sh noita` on a working
-# lutris Noita install.
 
 set -eu
-
-export MAIN_SAVE="$HOME/.steam/steam/steamapps/compatdata/881100/"
-mkdir -p $MAIN_SAVE
-
-# Environment variables
-export STEAM_COMPAT_APP_ID=777
-export STEAM_COMPAT_DATA_PATH="$WINEPREFIX"
-export STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.local/share/Steam
-export STEAM_COMPAT_LIBRARY_PATHS=$HOME/.local/share/Steam/steamapps
-export STEAM_RUNTIME_LIBRARY_PATH=$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_32:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_64:/usr/lib/libfakeroot:/usr/lib32:/usr/lib/perf:/usr/lib:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib/i386-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib/x86_64-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/x86_64-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib
-
-export WINEPREFIX=$WINEPREFIX/pfx
-
-# TODO: Shader cache?
 
 function log {
     echo $(date --rfc-3339=seconds) RUN_NOITA: $@
 }                                                                                                                                                            
-# Restore the games state and config from the main save and the golden config
-# respectively.
-if [[ "$WINEPREFIX" == "$MAIN_SAVE" ]]; then
-    :
-else
-    log "Setting up prefix: $WINEPREFIX"
-    mkdir -p $(dirname $WINEPREFIX)
-    rm -rf $WINEPREFIX
-    cp -r $MAIN_SAVE $WINEPREFIX
-fi
 
-mkdir -p $WINEPREFIX/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save_shared
-rm -rf $WINEPREFIX/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save0*
+export GAME_COMPAT_DATA="$HOME/.steam/steam/steamapps/compatdata/881100/"
+export RUN_COMPAT_DATA="$ENV_PREFIX/compatdata"
+export PROTON_ROOT="$ENV_PREFIX/compatdata/pfx"
+
+log "Setting up Noita game files at: $PROTON_ROOT"
+rm -rf $PROTON_ROOT
+mkdir -p $(dirname $PROTON_ROOT)
+cp -r $GAME_COMPAT_DATA $RUN_COMPAT_DATA 
+
+mkdir -p $PROTON_ROOT/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save_shared
+rm -rf $PROTON_ROOT/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save0*
 cp -f bounce_rl/environments/noita/mod/golden_config.xml \
-  $WINEPREFIX/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save_shared/config.xml
-# Copy a minimal save file into the environment to disable the intro screen.
+  $PROTON_ROOT/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save_shared/config.xml
 cp -r bounce_rl/environments/noita/initial_save00 \
-      $WINEPREFIX/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save00
+      $PROTON_ROOT/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save00
 ln -fs $(pwd)/bounce_rl/environments/noita/mod $HOME/.steam/steam/steamapps/common/Noita/mods/rl_mod
 
+# Set steam environment variables
+export STEAM_COMPAT_DATA_PATH="$RUN_COMPAT_DATA"
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.local/share/Steam
+export STEAM_COMPAT_LIBRARY_PATHS=$HOME/.local/share/Steam/steamapps
+export STEAM_RUNTIME_LIBRARY_PATH=$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_32:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_64:/usr/lib/libfakeroot:/usr/lib32:/usr/lib/perf:/usr/lib:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib/i386-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib/x86_64-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/x86_64-linux-gnu:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/lib:/home/william/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib
+# TODO: Shader cache?
 
+
+# Launch the game
 log "Running on display: $DISPLAY"
 log "Running offset: $PID_OFFSET"
 for i in $(seq 1 $PID_OFFSET); do
     touch "/tmp/noop.txt"
 done
-
 
 # Working Directory
 cd $HOME/.steam/steam/steamapps/common/Noita
