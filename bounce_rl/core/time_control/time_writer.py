bea31671879a0b690b9b97ffd9ae3c8348fff27b
William Henning <wchenningwchenning@gmail.com>
2024-05-11 21:14:10 -0400

    Delete unused main script in time_writer.py

--- bounce_rl/core/time_control/time_writer.py
+++ bounce_rl/core/time_control/time_writer.py
@@ -1,18 +1,18 @@
-# Call SetSpeedup() to change the time acceleration multiple for client programs listening on
-# the given channel.
+# Call SetSpeedup() to change the time acceleration multiple for client programs
+# listening on the given channel.
 
-import subprocess
-import time
-import struct
 import posix
+import struct
 
 FIFO = "/tmp/time_control"
 f = None
 
-def SetSpeedup(speedup, channel = ""):
+
+def SetSpeedup(speedup, channel=""):
     global f
     speedup = float(speedup)
-    # Opening the file for reading and writing prevents blocking until a reader opens the file.
+    # Opening the file for reading and writing prevents blocking until a reader opens
+    # the file.
     if f is None:
         print("Writing time to file: ", FIFO + str(channel))
         f = posix.open(FIFO + str(channel), posix.O_RDWR | posix.O_CREAT)
@@ -20,22 +20,3 @@ def SetSpeedup(speedup, channel = ""):
     SEEK_SET = 0
     posix.lseek(f, 0, SEEK_SET)
     posix.write(f, struct.pack("f", speedup))
-
-# Run a test if this file is directly invoked.
-if __name__ == "__main__":
-    for speedup, channel in [(12, 0), (18, 1)]:
-        SetSpeedup(speedup, channel)
-        environment = {"LD_PRELOAD": "../build/time_control.so",
-                       "TIME_CHANNEL": str(channel)}
-        proc = subprocess.Popen(["./a.out"], \
-                                env = environment,
-                                stdout = subprocess.PIPE)
-        time.sleep(1)
-        proc.kill()
-        out = proc.stdout.read()
-
-        output = out.decode("UTF-8")
-        print(output)
-        ticks = output.count("tick")
-        assert speedup - 1 <= ticks <= speedup + 1, f"WARNING: Time control sleep functions aren't implementd. Set speed-up to {speedup} but only hit {ticks} ticks."
-    print("Time writer tests passed!")
