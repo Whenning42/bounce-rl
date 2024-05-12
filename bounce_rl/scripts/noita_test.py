f140ddf25f12300e0e172e180dff43f5996f15c3
William Henning <wchenningwchenning@gmail.com>
2024-05-11 21:28:59 -0400

    Add seed and manual_control flags to noita_test

--- bounce_rl/scripts/noita_test.py
+++ bounce_rl/scripts/noita_test.py
@@ -1,20 +1,38 @@
+import argparse
+import time
+
 import numpy as np
 
 from bounce_rl.environments.noita import noita_env
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=13)
+    parser.add_argument("--manual_control", type=bool, default=False)
+    args = parser.parse_args()
+
+    xproxy = True
+    step = True
+    if args.manual_control:
+        xproxy = False
+        step = False
+
     noita_env.NoitaEnv.pre_init()
-    env = noita_env.NoitaEnv()
+    env = noita_env.NoitaEnv(
+        run_config={"run_rate": 1, "use_x_proxy": xproxy}, seed=args.seed
+    )
 
     terminated = False
     while True:
-        ret = env.step(
-            env.action_space.sample()
-        )
+        if not step:
+            time.sleep(10)
+            continue
+
+        ret = env.step(env.action_space.sample())
         if ret is not None:
             pixels, reward, done, info = ret
             print(
                 f"mean_pixels: {np.mean(pixels)}, r: {reward}, done: {done}, info {info}"
             )
-        if terminated:
+        if done:
             env.reset()
