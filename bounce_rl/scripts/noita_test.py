a849334755b91caad46876bcd04848af944d7ec0
William Henning <wchenningwchenning@gmail.com>
2024-05-16 19:06:06 -0400

    Switch noita_test.py to run parallel environment instances

--- bounce_rl/scripts/noita_test.py
+++ bounce_rl/scripts/noita_test.py
@@ -1,28 +1,20 @@
 import argparse
+import threading
 import time
 
 import numpy as np
 
 from bounce_rl.environments.noita import noita_env
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, default=13)
-    parser.add_argument("--manual_control", type=bool, default=False)
-    args = parser.parse_args()
 
-    xproxy = True
-    step = True
-    if args.manual_control:
-        xproxy = False
-        step = False
-
-    noita_env.NoitaEnv.pre_init()
+def run_env(instance: int):
     env = noita_env.NoitaEnv(
-        run_config={"run_rate": 1, "use_x_proxy": xproxy}, seed=args.seed
+        instance=instance,
+        x_pos=instance,
+        run_config={"run_rate": 1, "use_x_proxy": xproxy},
+        seed=args.seed,
     )
 
-    terminated = False
     while True:
         if not step:
             time.sleep(10)
@@ -36,3 +28,24 @@ if __name__ == "__main__":
             )
         if done:
             env.reset()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=13)
+    parser.add_argument("--manual_control", type=bool, default=False)
+    args = parser.parse_args()
+
+    xproxy = True
+    step = True
+    if args.manual_control:
+        xproxy = False
+        step = False
+
+    noita_env.NoitaEnv.pre_init(num_envs=2)
+    env_t1 = threading.Thread(target=run_env, args=(0,))
+    env_t2 = threading.Thread(target=run_env, args=(1,))
+    env_t1.start()
+    env_t2.start()
+    env_t1.join()
+    env_t2.join()
