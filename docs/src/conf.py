8a29afb1d112022de165e017b865be4fcd5dceb1
William Henning <wchenningwchenning@gmail.com>
2024-06-24 17:55:37 -0400

    Switch to read the docs theme

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,5 +29,7 @@ language = "en"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
