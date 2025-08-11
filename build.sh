4c3169c8c39064c016a217f4a87530f9975029bf
William Henning <wchenningwchenning@gmail.com>
2025-08-11 16:40:54 -0400

    Move libtimecontrol out of tree and into a library dependency

--- build.sh
+++ build.sh
@@ -22,14 +22,11 @@ cd ..
 cd meson_build
 rm -rf ../bounce_rl/libs
 mkdir ../bounce_rl/libs
-find . -name "*.so" | grep -v "libtime_control*" | xargs -I '{}' cp --parents {} ../
-find . -name "*.so" | grep "libtime_control*" | xargs -I '{}' cp --parents {} ../bounce_rl/libs
-cp bounce_rl/core/time_control/libtime_control.so ../bounce_rl/libs/libtime_control64.so
-cp bounce_rl/core/time_control/libtime_control32.so ../bounce_rl/libs/libtime_control32.so
+find . -name "*.so" | xargs -I '{}' cp --parents {} ../
 cd ..
 
 # Build python package
-for PYTHON_VERSION in 38 39 310 311; do
+for PYTHON_VERSION in 39 310 311; do
     poetry env use "/opt/python/cp${PYTHON_VERSION}-cp${PYTHON_VERSION}/bin/python"
     poetry build
 done
