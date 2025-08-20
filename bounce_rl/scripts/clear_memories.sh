38bbd38b3667f41f3fc60bfb61d4a15965cf0f1d
William Henning <wchenningwchenning@gmail.com>
2025-08-19 22:17:43 -0400

    Delete unused scripts

--- bounce_rl/scripts/clear_memories.sh
+++
@@ -1,2 +0,0 @@
-# Having this as a script reduces my chance of running a mistaken rm -r command
-rm -r memories/*
--- bounce_rl/scripts/ld_run.sh
+++
@@ -1,2 +0,0 @@
-# Runs the given command with the correct ld library path configured
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./:./build/lib PYTHONPATH=../stable-baselines3:$PYTHONPATH "$@"
