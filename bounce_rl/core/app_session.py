7932190a57e3ae3f00ab578291a7a4163917a256
William Henning <wchenningwchenning@gmail.com>
2025-10-17 16:04:53 -0400

    Update AppSession interface to allow setting up session data

--- bounce_rl/core/app_session.py
+++ bounce_rl/core/app_session.py
@@ -2,13 +2,26 @@
 # For each running instance of an app, app session holds a data folder, a virtual
 # desktop, and a process.
 
+import subprocess
+
 
 class AppSession:
-    def __init__(self):
+    def __init__(self, run_command: str):
+        """Creates an AppSession that will run `run_command` once .start_process() is
+        called."""
+        self._run_command = run_command
         self._desktop = None
         self._folder = None
         self._process = None
         self._time_controller = None
+
+    def start_process() -> subprocess.Process:
+        """Launch `run_command` in this AppSession.
+
+        This is it's own function instead of happening automatically in __init__,
+        so that callers can copy run-time data into the session folder before
+        `run_command` is run."""
+        pass
 
     @property
     def desktop(self):
